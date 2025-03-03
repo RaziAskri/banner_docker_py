a9525b8af1f0e09bf8444bd50cb75775fdde219f
RaziAskri <askrirazi@gmail.com>
2025-03-03 12:43:18 +0100

    update banner.py

--- banner.py
+++ banner.py
@@ -4,7 +4,7 @@ def print_intro():
     banner = """\033[1;34m============================================\033[0m
 \033[1;32m🌍 Hello world,\033[0m
 \033[1;36m👋 I'm Razi, a Cloud and DevOps Engineer.\033[0m
-\033[1;33m⚙️  Passionate about Cloud Tech & automation.\033[0m
+\033[1;33m⚙️ Passionate about Cloud Tech & automation.\033[0m
 \033[1;35m😊 Very happy to know you!\033[0m
 \033[1;34m============================================\033[0m"""
     
